89f86e42e7931e5a0a08c05ba5dc11898a88fa75
Chutiyababa <crazy.developers7@gmail.com>
2017-10-29 00:39:58 +0530

    Final

---
+++ ChutiyaBaba/Q1/src/com/rmiinterface/MS20172116_Interface.java
@@ -0,0 +1,15 @@
+package com.rmiinterface;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+
+import com.rmiserver.Transaction;
+
+public interface MS20172116_Interface extends Remote{
+	public String deposit(int number, double amount) throws RemoteException;
+	public String withdraw(int number, double amount) throws RemoteException;
+	public String balance(int number) throws RemoteException;
+	public java.util.List<Transaction> transaction(int number, Date fDate, Date toDate) throws RemoteException;
+
+}
